908d96e75ff4ce24487e28ef1caf5ed5ffbd43e5
habakkukyong <habakkukyong@gmail.com>
2019-01-14 14:56:51 +0000

    passing top up test

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,4 +4,10 @@ describe Oystercard do
   it 'should return balance is 0' do
     expect(subject.balance).to eq 0
   end
+  describe '#top_up' do
+    it 'should increase balance by an amount' do
+      subject.top_up(10)
+      expect(subject.balance).to eq 10
+    end
+  end
 end
